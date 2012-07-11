f83a6d7fbaa7f6b3e01defc5a0f3164743826e3e
Hugh Lv <hugh.lv@gmail.com>
2012-07-11 18:48:39 +0800

    Remove redundant files.

--- demo/Demo/main.m
+++ demo/Demo/main.m
@@ -15,8 +15,8 @@ int main(int argc, char *argv[])
 {
     // You can init your key info here.
     
-    //QBOX_ACCESS_KEY = "<Please apply your access key>";
-    //QBOX_SECRET_KEY = "<Dont send your secret key to anyone>";
+    QBOX_ACCESS_KEY = "<Please apply your access key>";
+    QBOX_SECRET_KEY = "<Dont send your secret key to anyone>";
     
     @autoreleasepool {
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([DPAppDelegate class]));
