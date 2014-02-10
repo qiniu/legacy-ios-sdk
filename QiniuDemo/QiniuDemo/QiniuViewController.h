28bfaf9ed6750b691c302a49997eb6d3e0105b39
柱石 <zhushi@alibaba-inc.com>
2014-02-10 14:13:40 +0800

    ci

--- QiniuDemo/QiniuDemo/QiniuViewController.h
+++ QiniuDemo/QiniuDemo/QiniuViewController.h
@@ -6,7 +6,7 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "../../QiniuSDK/QiniuSimpleUploader.h"
+#import "QiniuSimpleUploader.h"
 
 @interface QiniuViewController : UIViewController<QiniuUploadDelegate, UINavigationControllerDelegate, UIPopoverControllerDelegate, UIImagePickerControllerDelegate>
 {
