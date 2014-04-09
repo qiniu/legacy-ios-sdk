533c541112f593453f574dd971979eb24f14e687
longbai <slongbai@gmail.com>
2014-04-09 23:03:03 +0800

    update to 2.2.1

--- QiniuSDK/AFNetwork2/AFHTTPRequestOperationManager.m
+++ QiniuSDK/AFNetwork2/AFHTTPRequestOperationManager.m
@@ -34,13 +34,16 @@
 
 @interface AFHTTPRequestOperationManager ()
 @property (readwrite, nonatomic, strong) NSURL *baseURL;
-@property (readwrite, nonatomic, strong) AFNetworkReachabilityManager *reachabilityManager;
 @end
 
 @implementation AFHTTPRequestOperationManager
 
 + (instancetype)manager {
     return [[[self class] alloc] initWithBaseURL:nil];
+}
+
+- (instancetype)init {
+    return [self initWithBaseURL:nil];    
 }
 
 - (instancetype)initWithBaseURL:(NSURL *)url {
@@ -61,15 +64,11 @@
 
     self.securityPolicy = [AFSecurityPolicy defaultPolicy];
 
-    if (self.baseURL.host) {
-        self.reachabilityManager = [AFNetworkReachabilityManager managerForDomain:self.baseURL.host];
-    } else {
-        self.reachabilityManager = [AFNetworkReachabilityManager sharedManager];
-    }
-
-    [self.reachabilityManager startMonitoring];
+    self.reachabilityManager = [AFNetworkReachabilityManager sharedManager];
 
     self.operationQueue = [[NSOperationQueue alloc] init];
+
+    self.shouldUseCredentialStorage = YES;
 
     return self;
 }
