a8778fc2efefb8f8449305aa1acbde9484652b48
lintianzhi <lintianzhi1992@gmail.com>
2014-05-16 15:23:22 +0800

    reqid

--- QiniuSDK/QiniuUtils.m
+++ QiniuSDK/QiniuUtils.m
@@ -47,9 +47,11 @@ NSError *qiniuNewErrorWithRequest(ASIHTTPRequest *request) {
         errorDescription = [httpError localizedDescription];
     }
     
-    NSDictionary *userInfo = nil;
+    NSString *reqid = [[request responseHeaders] objectForKey:@"X-Reqid"];
+    NSMutableDictionary *userInfo = [NSMutableDictionary dictionaryWithObject:reqid forKey:@"reqid"];
+    
     if (errorDescription) {
-        userInfo = [NSDictionary dictionaryWithObject:errorDescription forKey:kQiniuErrorKey];
+        [userInfo setObject:errorDescription forKey:kQiniuErrorKey];
     }
     
     return [NSError errorWithDomain:kQiniuErrorDomain code:errorCode userInfo:userInfo];
