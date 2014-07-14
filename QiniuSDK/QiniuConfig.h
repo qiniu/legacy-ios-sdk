5270f07b640633a0303f380d485bf0bf783429b2
lintianzhi <lintianzhi1992@gmail.com>
2014-07-14 23:11:26 +0800

    x

--- QiniuSDK/QiniuConfig.h
+++ QiniuSDK/QiniuConfig.h
@@ -8,8 +8,8 @@
 #import <Foundation/Foundation.h>
 
 #define kQiniuUpHost @"http://upload.qiniu.com"
-#define kQiniuUpHost2 @"http://up.qbox.me"
+#define kQiniuUpHost2 @"http://up.qiniu.com"
 
 #define kQiniuUndefinedKey @"?"
 
-#define kQiniuVersion @"6.2.7"
+#define kQiniuVersion @"6.3.0"
